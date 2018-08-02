5e13ec142128d13dcf6d8718e976cf545ee2ee09
Mihkel Veske <veskem@gmail.com>
2018-08-02 22:30:02 +0300

    start implementing force export

--- src/USER-FEMOCS/fix_femocs.cpp
+++ src/USER-FEMOCS/fix_femocs.cpp
@@ -11,18 +11,38 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
+#include <cstring>
+#include <cstdlib>
 #include "fix_femocs.h"
-#include "atom.h"
-#include "math_extra.h"
 #include "comm.h"
+#include "math_extra.h"
+#include "atom.h"
+#include "atom_masks.h"
+#include "update.h"
+#include "modify.h"
+#include "domain.h"
+#include "region.h"
+#include "respa.h"
+#include "input.h"
+#include "variable.h"
+#include "memory.h"
+#include "error.h"
+#include "force.h"
+
 
 using namespace LAMMPS_NS;
+
+enum{NONE,CONSTANT,EQUAL,ATOM};
 
 /* ---------------------------------------------------------------------- */
 
 FixFemocs::FixFemocs(LAMMPS *lmp, int narg, char **arg) :
-        Fix(lmp, narg, arg), femocs("in.fem")
+        Fix(lmp, narg, arg)
 {
+    if (narg != 4)
+        error->all(FLERR,"Illegal fix femocs command");
+
+    femocs.read_conf(arg[3]);
 }
 
 /* ---------------------------------------------------------------------- */
@@ -32,32 +52,149 @@ FixFemocs::~FixFemocs()
 
 /* ---------------------------------------------------------------------- */
 
+void FixFemocs::print(char* msg)
+{
+    if (comm->me == 0) printf("FixFemocs: %s\n", msg);
+}
+
+/* ---------------------------------------------------------------------- */
+
 int FixFemocs::setmask()
 {
+  datamask_read = datamask_modify = 0;
+
   int mask = 0;
-  mask |= FixConst::END_OF_STEP;
+  mask |= FixConst::POST_FORCE;
+  mask |= FixConst::THERMO_ENERGY;
+  mask |= FixConst::POST_FORCE_RESPA;
+  mask |= FixConst::MIN_POST_FORCE;
   return mask;
 }
 
-void FixFemocs::end_of_step()
+/* ---------------------------------------------------------------------- */
+
+void FixFemocs::post_force(int vflag)
 {
-  // for add3, scale3
-  using namespace MathExtra;
-  double** v = atom->v;
-  int nlocal = atom->nlocal;
-  double localAvgVel[4]; // 4th element for particles count
-  memset(localAvgVel, 0, 4 * sizeof(double));
-  for (int particleInd = 0; particleInd < nlocal; ++particleInd) {
-    if (atom->mask[particleInd] & groupbit)
-      add3(localAvgVel, v[particleInd], localAvgVel);
+    require(comm->nprocs == 1, "fix femocs can run only on single CPU core!");
+
+    double *xyz_1d = &atom->x[0][0];
+    double *velocity_1d = &atom->v[0][0];
+    double *force_1d = &atom->f[0][0];
+
+    atom->nmax;
+
+    int n_atoms = atom->nlocal;
+
+    // import atomistic data to Femocs
+    // NB! Atom indices change between time steps due to sorting
+    //     Maybe storing atoms helps as described in Developer.pdf
+    if (femocs.import_atoms(n_atoms, xyz_1d)) {
+        print("importing atoms failed!");
+        return;
+    }
+
+    // solve equations
+    if (femocs.run()) {
+        print("solving equations failed!");
+        return;
+    }
+
+    // export forces
+    if (femocs.export_data(force_1d, n_atoms, "force")) {
+        print("exporting forces failed!");
+        return;
+    }
+
+    // TODO handle energies
+
+//    // export total potential energy of atoms without contribution of Lorentz interaction
+//    success += femocs.export_data(Epair_all, 1, "pair_potential_sum");
+//    Vpair = Epair_all[0];
+//    // export potential energy per atom (pair-potential)
+//    success += femocs.export_data(Epair_all, n_atoms, "pair_potential");
+    //    // scale velocities
+//    success += femocs.export_data(v, n_atoms, "velocity");
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixFemocs::init()
+{
+  // TODO check what's going on here
+
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixFemocs::setup(int vflag)
+{
+  if (strstr(update->integrate_style,"verlet"))
+    post_force(vflag);
+  else {
+    ((Respa *) update->integrate)->copy_flevel_f(ilevel_respa);
+    post_force_respa(vflag,ilevel_respa,0);
+    ((Respa *) update->integrate)->copy_f_flevel(ilevel_respa);
   }
-  localAvgVel[3] = nlocal;
-  double globalAvgVel[4];
-  memset(globalAvgVel, 0, 4 * sizeof(double));
-  MPI_Allreduce(localAvgVel, globalAvgVel, 4, MPI_DOUBLE, MPI_SUM, world);
-  scale3(1.0 / globalAvgVel[3], globalAvgVel);
-  if (comm->me == 0) {
-    printf("globalAvgVel = \%e, \%e, \%e\n",
-        globalAvgVel[0], globalAvgVel[1], globalAvgVel[2]);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixFemocs::min_setup(int vflag)
+{
+  post_force(vflag);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixFemocs::post_force_respa(int vflag, int ilevel, int iloop)
+{
+  if (ilevel == ilevel_respa) post_force(vflag);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixFemocs::min_post_force(int vflag)
+{
+  post_force(vflag);
+}
+
+/* ----------------------------------------------------------------------
+   potential energy of added force
+------------------------------------------------------------------------- */
+
+double FixFemocs::compute_scalar()
+{
+  // only sum across procs one time
+
+  if (force_flag == 0) {
+    MPI_Allreduce(foriginal,foriginal_all,4,MPI_DOUBLE,MPI_SUM,world);
+    force_flag = 1;
   }
+  return foriginal_all[0];
+}
+
+/* ----------------------------------------------------------------------
+   return components of total force on fix group before force was changed
+------------------------------------------------------------------------- */
+
+double FixFemocs::compute_vector(int n)
+{
+  // only sum across procs one time
+
+  if (force_flag == 0) {
+    MPI_Allreduce(foriginal,foriginal_all,4,MPI_DOUBLE,MPI_SUM,world);
+    force_flag = 1;
+  }
+  return foriginal_all[n+1];
+}
+
+/* ----------------------------------------------------------------------
+   memory usage of local atom-based array
+------------------------------------------------------------------------- */
+
+double FixFemocs::memory_usage()
+{
+  double bytes = 0.0;
+  if (varflag == ATOM) bytes = maxatom*4 * sizeof(double);
+  return bytes;
 }
--- src/USER-FEMOCS/fix_femocs.h
+++ src/USER-FEMOCS/fix_femocs.h
@@ -25,10 +25,34 @@ public:
   virtual ~FixFemocs();
 
   int setmask();
-  void end_of_step();
+  void init();
+  void setup(int);
+  void min_setup(int);
+  void post_force(int);
+  void post_force_respa(int, int, int);
+  void min_post_force(int);
+
+  double compute_scalar();
+  double compute_vector(int);
+  double memory_usage();
 
 protected:
   femocs::Femocs femocs;
+
+  // TODO clarify & clean them up
+
+  double xvalue,yvalue,zvalue;
+  int varflag,iregion;
+  char *xstr,*ystr,*zstr,*estr;
+  char *idregion;
+  int xvar,yvar,zvar,evar,xstyle,ystyle,zstyle,estyle;
+  double foriginal[4],foriginal_all[4];
+  int force_flag;
+  int ilevel_respa;
+  int maxatom;
+  double **sforce;
+
+  void print(char* msg);
 };
 
 }
